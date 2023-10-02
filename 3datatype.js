4fd55ebbdd830439d3d1684ca23cea26b64b078f
Saheb ali <130155229+techsaheb@users.noreply.github.com>
2023-10-03 02:30:50 +0300

    Add files via upload

---
+++ 3datatype.js
@@ -0,0 +1,70 @@
+// javascript data type
+
+// javascript has 8 type of data 
+/*primative data
+  1.string
+  2.Number
+  3.boolean
+  4.undefine 
+  5.null
+  6.bigint
+  7.symble
+  8.object
+
+// object type data 
+    1.function
+    2.array
+    3.object    */
+
+
+    // primative data type 
+    // 1.example string data type 
+    let nam = "saheb"; // declared with double quote
+    let surname = 'ali'; // declared with single quote
+    let lastname = String("ali khan"); // declared  with string conestratore
+    let firstname = `mr ali` // declared with back tik
+
+
+    //2.example number data type 
+    let number = 10;
+    let decimal = 10.10;
+    let number1 = Number("100");
+
+
+    // 3. example boolean data type
+
+    let yes = true;
+    let no = false;
+    let yes1 = Boolean("true")
+
+    //4.undefine data type
+    let ali ;
+    console.log(typeof(ali));
+    
+
+    //null data type 
+    let nul = null;
+
+
+
+
+
+    // object type data type
+    // 1. function data type
+    function add (a,b){
+        console.log(a+b);
+    };
+
+
+    // 2.array 
+    let cars = ["bmw", "volvo", "ferari"];
+
+    //3.object
+    let obj = {
+        name : "saheb",
+        surname : "ali",
+
+    };
+
+
+
